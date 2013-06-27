1ceaf0a790577d71a86385210ce6c3959d96ec71
= <=>
2013-06-27 17:13:29 -0500

    Console client in C#

---
+++ console_client/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Services;
+using ConsoleApplication1.simplecrud;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            simplecrud.simpleService ws = new simplecrud.simpleService();
+            //string mensaje = ws.conexionMysqlcreate("roberto","33333","localhost","megaz","m@m.com");
+            //string mensaje = ws.conexionMysqldelete(131);
+            //string mensaje = ws.conexionMysqlupdate(131, "desdec#", "1111", "ninguna");
+            string mensaje = ws.conexionMysqlread();
+            Console.WriteLine(mensaje);
+            Console.ReadLine();
+        }
+    }
+}
